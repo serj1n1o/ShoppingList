abd6dd9eca74908e3da15213d687ad7c62ed11df
Eka666Sed <cat.sedoff@gmail.com>
2024-12-09 14:37:59 +0100

    21_удаляет неиспользуемый импорт

--- app/src/main/java/com/bryukhanov/shoppinglist/mylists/presentation/view/MyListsFragment.kt
+++ app/src/main/java/com/bryukhanov/shoppinglist/mylists/presentation/view/MyListsFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bryukhanov.shoppinglist.R
@@ -54,7 +53,7 @@ class MyListsFragment : Fragment() {
     private fun showCustomDialog() {
         val dialog = Dialog(requireContext(), R.style.CustomDialogTheme)
         val dialogBinding =
-            LayoutCustomDialogBinding.inflate(layoutInflater) // Используйте ViewBinding для layout_custom_dialog
+            LayoutCustomDialogBinding.inflate(layoutInflater)
         dialog.setContentView(dialogBinding.root)
 
         dialogBinding.tvDialogMessage.text = getString(R.string.dialog_message)
