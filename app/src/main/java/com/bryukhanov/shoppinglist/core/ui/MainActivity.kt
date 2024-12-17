43f1d51c7e7d416403c8acdeaa0de3087d3b36dd
Eka666Sed <cat.sedoff@gmail.com>
2024-12-17 17:26:36 +0100

    25_добавляет возможность менять и сохрянять тему приложения

--- app/src/main/java/com/bryukhanov/shoppinglist/core/ui/MainActivity.kt
+++ app/src/main/java/com/bryukhanov/shoppinglist/core/ui/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bryukhanov.shoppinglist.R
+import com.bryukhanov.shoppinglist.core.util.ThemeManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        ThemeManager.applyTheme(this)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(
